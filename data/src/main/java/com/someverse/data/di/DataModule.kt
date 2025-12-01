c0a81a89568d22e5f5b5b43f1513ce569e794179
keem-hyun <hw813@naver.com>
2025-12-02 00:09:49 +0900

    feat: di - data module

--- data/src/main/java/com/someverse/data/di/DataModule.kt
+++ data/src/main/java/com/someverse/data/di/DataModule.kt
@@ -1,9 +1,13 @@
 package com.someverse.data.di
 
 import com.someverse.data.impl.AuthRepositoryImpl
+import com.someverse.data.impl.ChatRepositoryImpl
 import com.someverse.data.local.AuthLocalDataSource
+import com.someverse.data.local.ChatLocalDataSource
 import com.someverse.data.source.AuthDataSource
+import com.someverse.data.source.ChatDataSource
 import com.someverse.domain.repository.AuthRepository
+import com.someverse.domain.repository.ChatRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -33,6 +37,23 @@ object DataModule {
         // return AuthRemoteDataSource(authApiService)
     }
 
+    /**
+     * Provide ChatDataSource implementation
+     *
+     * SWITCH BETWEEN LOCAL AND REMOTE HERE:
+     * - Local (Mock): return ChatLocalDataSource()
+     * - Remote (API): return ChatRemoteDataSource(chatApiService)
+     */
+    @Provides
+    @Singleton
+    fun provideChatDataSource(): ChatDataSource {
+        // 현재: Local (Mock) 사용
+        return ChatLocalDataSource()
+
+        // 나중에 Remote로 교체:
+        // return ChatRemoteDataSource(chatApiService)
+    }
+
     // ==================== Repositories ====================
 
     @Provides
@@ -41,5 +62,13 @@ object DataModule {
         dataSource: AuthDataSource  // Interface 주입!
     ): AuthRepository {
         return AuthRepositoryImpl(dataSource)
+    }
+
+    @Provides
+    @Singleton
+    fun provideChatRepository(
+        dataSource: ChatDataSource  // Interface 주입!
+    ): ChatRepository {
+        return ChatRepositoryImpl(dataSource)
     }
 }
