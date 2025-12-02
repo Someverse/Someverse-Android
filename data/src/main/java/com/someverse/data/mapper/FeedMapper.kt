8100c14dc625926a4deae271f685dcfd0eecb930
keem-hyun <hw813@naver.com>
2025-12-03 00:27:32 +0900

    feat: feed mapper

---
+++ data/src/main/java/com/someverse/data/mapper/FeedMapper.kt
@@ -0,0 +1,64 @@
+package com.someverse.data.mapper
+
+import com.someverse.data.model.CreateFeedRequestDto
+import com.someverse.data.model.FeedEntity
+import com.someverse.data.model.UpdateFeedRequestDto
+import com.someverse.domain.model.Feed
+import com.someverse.domain.model.FeedType
+
+/**
+ * Feed Mapper
+ * - Converts between Entity (DTO) and Domain Model
+ * - Entity: Data layer (API/DB)
+ * - Domain: Business logic layer
+ */
+object FeedMapper {
+
+    /**
+     * Convert FeedEntity to Feed domain model
+     */
+    fun FeedEntity.toDomain(): Feed {
+        return Feed(
+            id = this.id,
+            content = this.content,
+            nickName = this.nickName,
+            profileImage = this.profileImages
+        )
+    }
+
+    /**
+     * Convert Feed domain model to FeedEntity
+     */
+    fun Feed.toEntity(): FeedEntity {
+        return FeedEntity(
+            id = this.id,
+            content = this.content,
+            nickName = this.nickName,
+            profileImages = this.profileImage
+        )
+    }
+
+    /**
+     * Convert domain parameters to CreateFeedRequestDto
+     */
+    fun createRequestDto(
+        feedType: FeedType,
+        movieId: Long?,
+        musicId: Long?,
+        content: String
+    ): CreateFeedRequestDto {
+        return CreateFeedRequestDto(
+            feedType = feedType.name,
+            movieId = movieId,
+            musicId = musicId,
+            content = content
+        )
+    }
+
+    /**
+     * Convert domain parameter to UpdateFeedRequestDto
+     */
+    fun updateRequestDto(content: String): UpdateFeedRequestDto {
+        return UpdateFeedRequestDto(content = content)
+    }
+}
