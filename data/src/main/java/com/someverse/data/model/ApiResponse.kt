bfb9c958f034a4ae9ab3d44c649e08023fd28b3b
keem-hyun <hw813@naver.com>
2025-12-22 22:35:23 +0900

    feat: data - base api response

---
+++ data/src/main/java/com/someverse/data/model/ApiResponse.kt
@@ -0,0 +1,22 @@
+package com.someverse.data.model
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Generic API Response wrapper
+ * - Standard response format from backend
+ * - Contains success status, code, message, and data
+ */
+data class ApiResponse<T>(
+    @SerializedName("success")
+    val success: Boolean,
+
+    @SerializedName("code")
+    val code: String,
+
+    @SerializedName("message")
+    val message: String,
+
+    @SerializedName("data")
+    val data: T?
+)
