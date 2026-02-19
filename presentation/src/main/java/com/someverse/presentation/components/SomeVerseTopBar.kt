b8c0f8dce658bfc6b464251bd17b6c4e968ef9c2
yjkwon <casper118412@gmail.com>
2026-02-19 16:00:05 +0900

    feat: add SomeVerseTopBar

---
+++ presentation/src/main/java/com/someverse/presentation/components/SomeVerseTopBar.kt
@@ -0,0 +1,103 @@
+package com.someverse.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.someverse.presentation.R
+import com.someverse.presentation.ui.theme.DescGray
+import com.someverse.presentation.ui.theme.PretendardFontFamily
+import com.someverse.presentation.ui.theme.withLetterSpacingPercent
+
+@Composable
+fun SomeVerseTopBar(
+    modifier: Modifier = Modifier,
+    title: String = "",
+    leadingIcon: (@Composable () -> Unit)? = null,
+    trailingIcon: (@Composable () -> Unit)? = null
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(height = 52.dp)
+            .background(color = Color(0xffFAFAFA))
+    ) {
+        // 좌측 아이콘
+        Box(
+            modifier = modifier
+                .padding(horizontal = 30.dp)
+                .align(Alignment.CenterStart)
+        ) {
+            leadingIcon?.invoke()
+        }
+        // 제목
+        Text(
+            text = title, style = MaterialTheme.typography.titleMedium
+                .copy(
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 20.sp,
+                    lineHeight = 32.sp,
+                    textAlign = TextAlign.Center,
+                    fontFamily = PretendardFontFamily,
+                ).withLetterSpacingPercent(-2.5f),
+            color = DescGray,
+            modifier = modifier.align(Alignment.Center)
+        )
+        // 우측 아이콘
+        Box(
+            modifier = modifier
+                .padding(horizontal = 30.dp)
+                .align(Alignment.CenterEnd)
+        ) {
+            trailingIcon?.invoke()
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun SomeVerseTopBarPreview() {
+    SomeVerseTopBar(
+        title = "취향 입력",
+        leadingIcon = {
+            IconButton(
+                onClick = {}
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_dismiss),
+                    contentDescription = "뒤로가기",
+                    tint = Color(0xFF9098A6),
+                    modifier = Modifier.size(size = 28.dp)
+                )
+            }
+        },
+        trailingIcon = {
+            IconButton(
+                onClick = {}
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_search),
+                    contentDescription = "신고",
+                    tint = Color(0xff9098A6),
+                    modifier = Modifier.size(size = 20.dp)
+                )
+            }
+        }
+    )
+}
